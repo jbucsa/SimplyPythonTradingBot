82fba8527dde77d34a2da7144bdfe1da2fa2fb2c
jbucsa <justin.bucsa@gmail.com>
2024-02-04 13:47:09 -0600

    Update TradingHistory.py file

--- TradingHistory.py
+++ TradingHistory.py
@@ -1,8 +1,16 @@
+#Run pip yfinance
+# Import from Yahoo Finance
 import yfinance as yf
+
+#Run pip pandas
 import pandas as pd
 
-dataF = yf.download("EURUSD=X", start="2024-02-1", end="2024-02-2", interval='15m')
+dataF = yf.download("EURUSD=X", start="2024-02-1", end="2024-02-05", interval='15m')
 dataF.iloc[:,:]
 dataF.Open.iloc
-print(dataF.iloc[:, :])  # Print the entire DataFrame
-selected_open = dataF.Open.iloc[:]  # Select Open column and store in a variable
+
+# Print the entire DataFrame
+print(dataF.iloc[:,:])
+
+# Select Open column and store in a variable
+selected_open = dataF.Open.iloc[:] 
